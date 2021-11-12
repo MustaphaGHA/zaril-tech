c683b89848d2767d53fb7f27b720178598e1d1a6
Mustapha Ghannouchi <mustapha.ghannouchi@esprit.tn>
2021-11-12 18:50:11 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import React , {useEffect , useState} from 'react'
 import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss'
+import {Layout} from '../components'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    
+    <Component {...pageProps} />
+
+  </Layout>
+  
+  
+  )
 }
 
 export default MyApp
